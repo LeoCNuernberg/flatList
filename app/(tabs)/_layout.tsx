ce46d4386b24cb74d0a59fc0c3b64b6575da44db
Léo <leo07nuernberg@gmail.com>
2026-09-17 11:42:34 -0300

    atualização pelo prof

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,67 +1,17 @@
-import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { Platform } from "react-native";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: "rgb(93, 0, 7)",
-          elevation: 0,
-          shadowOpacity: 0,
-          borderBottomWidth: 1,
-          borderBottomColor: "rgb(37, 6, 6)",
-        },
-        headerTitleStyle: {
-          fontWeight: "700",
-          fontSize: 18,
-          color: "rgb(254, 248, 248)",
-        },
-        tabBarActiveTintColor: "#007AFF",
-        tabBarInactiveTintColor: "#8E8E93",
+        headerShown: false,
         tabBarStyle: {
-          backgroundColor: "rgb(9, 9, 94)",
-          borderTopWidth: 1,
-          borderTopColor: "#F0F0F0",
-          height: Platform.OS === "ios" ? 88 : 64,
-          paddingBottom: Platform.OS === "ios" ? 30 : 80,
-          paddingTop: 10,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontWeight: "500",
-        },
+          display: "none"
+        }
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Início",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "link" : "eye"}
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />    
-      <Tabs.Screen
-        name="filme"
-        options={{
-          title: "TITULO FILME",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "list" : "list-outline"}
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />        
-       
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="filme" />
     </Tabs>
   );
 }
